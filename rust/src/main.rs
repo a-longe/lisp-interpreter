4dd2bfdc9ca91e79afc9ec1ecc28fabab9c7c13f
Alex Longe <alex.m.longe@gmail.com>
2024-09-02 20:06:32 -0700

    added first parser function and tests

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,6 @@
+pub mod parse;
+pub mod tests;
+
 fn main() {
     println!("Hello World!");
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn passing_test() {
-        assert_eq!(1, 1)
-    }
 }
--- rust/src/parse.rs
+++ rust/src/parse.rs
@@ -1,3 +1,17 @@
+use core::panic;
+
 pub fn get_string_between_parenteses(starting_parentheses_index: usize, s: &str) -> &str {
-    "test"
+    let mut opening_parens = 0;
+    for (i, c) in s[starting_parentheses_index..].chars().enumerate() {
+        println!("{}", c);
+        match c {
+            '(' => opening_parens += 1,
+            ')' => opening_parens -= 1,
+            _ => continue,
+        }
+        if opening_parens == 0 {
+            return &s[starting_parentheses_index..=i + starting_parentheses_index];
+        }
+    }
+    panic!("invalid string / starting index")
 }
